69dc030fb533212dc588beb10791e074f74b9499
Vladislav Sufyanov <sufyanovvlad@gmail.com>
2021-01-02 23:05:25 +0300

    Change config

--- app/core/config.py
+++ app/core/config.py
@@ -1,8 +1,26 @@
-from pydantic import BaseSettings, PostgresDsn
+from typing import Optional, Dict, Any
+
+from pydantic import BaseSettings, PostgresDsn, validator
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: PostgresDsn
+    POSTGRES_SERVER: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    DATABASE_URL: Optional[PostgresDsn] = None
+
+    @validator('DATABASE_URL', pre=True)
+    def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
+        if isinstance(v, str):
+            return v
+        return PostgresDsn.build(
+            scheme='postgresql',
+            user=values.get('POSTGRES_USER'),
+            password=values.get('POSTGRES_PASSWORD'),
+            host=values.get('POSTGRES_SERVER'),
+            path=f'/{values.get("POSTGRES_DB") or ""}',
+        )
 
     class Config:
         case_sensitive = True
