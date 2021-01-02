5f2558958c04e322a2ff7eb63e6db55ff9c59fe1
Vladislav Sufyanov <sufyanovvlad@gmail.com>
2021-01-03 00:17:50 +0300

    Change postgresql env in config

--- app/core/config.py
+++ app/core/config.py
@@ -1,9 +1,13 @@
 from typing import Optional, Dict, Any
+import secrets
 
 from pydantic import BaseSettings, PostgresDsn, validator
 
 
 class Settings(BaseSettings):
+    API_V1_STR: str = '/api/v1'
+    SECRET_KEY: str = secrets.token_urlsafe(32)
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24
     POSTGRES_SERVER: str
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
