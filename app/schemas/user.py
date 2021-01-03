5ec14b18b18eed5b9ed4037f1caf6f784364a4b0
Vladislav Sufyanov <sufyanovvlad@gmail.com>
2021-01-03 22:58:43 +0300

    Add constraints to pydantic scheme user

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,12 +1,12 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class BaseUser(BaseModel):
-    username: str
+    username: str = Field(..., example='user', min_length=1, max_length=50)
 
 
 class CreateUser(BaseUser):
-    plain_password: str
+    plain_password: str = Field(..., example='32ptyS69Ise3mquNTavs', min_length=5, max_length=50)
 
 
 class User(BaseUser):
