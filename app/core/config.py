d6ad974af464e43bcf055fd7d28f44962e780b9a
Vladislav Sufyanov <sufyanovvlad@gmail.com>
2021-01-03 17:05:23 +0300

    Add project name into config

--- app/core/config.py
+++ app/core/config.py
@@ -5,6 +5,7 @@ from pydantic import BaseSettings, PostgresDsn, validator
 
 
 class Settings(BaseSettings):
+    PROJECT_NAME: str = 'Simple Post app'
     API_V1_STR: str = '/api/v1'
     SECRET_KEY: str = secrets.token_urlsafe(32)
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24
