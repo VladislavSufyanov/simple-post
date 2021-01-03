855312d0397f7bd3be13dcfac0fe12892d6028b6
Vladislav Sufyanov <sufyanovvlad@gmail.com>
2021-01-03 17:06:30 +0300

    Alter column username to unique

--- app/models/user.py
+++ app/models/user.py
@@ -6,6 +6,6 @@ users = sqlalchemy.Table(
     'users',
     metadata,
     sqlalchemy.Column('id', sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Column('username', sqlalchemy.String),
+    sqlalchemy.Column('username', sqlalchemy.String, unique=True),
     sqlalchemy.Column('password', sqlalchemy.String)
 )
