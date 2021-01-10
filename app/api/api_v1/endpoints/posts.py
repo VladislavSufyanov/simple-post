b2b90c692dec28832006b9a3bb9cfc469f76b357
Vladislav Sufyanov <sufyanovvlad@gmail.com>
2021-01-11 01:05:32 +0300

    Add delete post

--- app/api/api_v1/endpoints/posts.py
+++ app/api/api_v1/endpoints/posts.py
@@ -1,10 +1,10 @@
 from typing import List
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 
 from api.dependencies.users import get_current_user
 from schemas.user import UserInDB
-from schemas.posts import ResponsePost, BasePost, CreatePost
+from schemas.posts import ResponsePost, BasePost, CreatePost, PostID
 from crud.posts import crud_post
 
 
@@ -20,4 +20,12 @@ async def get_user_posts(username: str, current_user: UserInDB = Depends(get_cur
 @router.post('/', response_model=ResponsePost)
 async def create_post(post: BasePost, current_user: UserInDB = Depends(get_current_user)):
     response_post = await crud_post.create(current_user.username, CreatePost(**post.dict(), user_id=current_user.id))
+    return response_post
+
+
+@router.delete('/{post_id}', response_model=PostID)
+async def delete_post(post_id: int, current_user: UserInDB = Depends(get_current_user)):
+    response_post = await crud_post.delete(post_id, current_user.id)
+    if response_post is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Post not found')
     return response_post
--- app/crud/posts.py
+++ app/crud/posts.py
@@ -1,19 +1,21 @@
-from typing import List
+from typing import List, Optional
 
-from sqlalchemy.sql.dml import Insert
+from sqlalchemy.sql.dml import Insert, Delete
 from sqlalchemy.sql import select
 from sqlalchemy.sql.selectable import Select
+from sqlalchemy.sql.operators import and_
 
 from crud.base import CRUD
 from db.db import db
 from models.post import posts
 from models.user import users
-from schemas.posts import CreatePost, ResponsePost
+from schemas.posts import CreatePost, ResponsePost, PostID
 
 
 class CRUDPosts(CRUD):
     async def get_user_posts(self, username: str) -> List[ResponsePost]:
-        query: Select = select([self.table.c.date_pub, self.table.c.title, self.table.c.text]). \
+        query: Select = select([self.table.c.id, self.table.c.date_pub,
+                                self.table.c.title, self.table.c.text]). \
             select_from(self.table.join(self.related_tables['users'])). \
             where(self.related_tables['users'].c.username == username). \
             order_by(self.table.c.date_pub.desc())
@@ -24,7 +26,16 @@ class CRUDPosts(CRUD):
         query: Insert = self.table.insert().returning(self.table.c.id, self.table.c.date_pub.label('date_pub'))
         returning_values = await db.fetch_one(query, post.dict())
         return ResponsePost(username=username, title=post.title, text=post.text,
-                            date_pub=returning_values['date_pub'])
+                            date_pub=returning_values['date_pub'], id=returning_values['id'])
+
+    async def delete(self, post_id: int, current_user_id: int) -> Optional[PostID]:
+        query: Delete = self.table.delete().\
+            where(and_(self.table.c.id == post_id, self.table.c.user_id == current_user_id)).\
+            returning(self.table.c.id)
+        deleted_post_id = await db.execute(query)
+        if deleted_post_id is None:
+            return
+        return PostID(id=deleted_post_id)
 
 
 crud_post = CRUDPosts(posts, {'users': users})
